2e5ac21d50dbceaf458a57d044623c0f9f1cbac6
FakeFu1ure <itsmelightbulb@gmail.com>
2026-02-19 21:23:59 +0300

    Update anidub.py

--- parsers/anidub.py
+++ parsers/anidub.py
@@ -3,29 +3,30 @@ from bs4 import BeautifulSoup
 import lxml
 import re
 
-response = requests.get('https://tr.anidub.com/anime_tv/full/page/1/')
-soup = BeautifulSoup(response.text, 'lxml')
-anime_links = soup.select('div.lcol a[href*="/anime_tv/full/"]')
+number = 1
+for i in range(1,136):
+    response = requests.get(f'https://tr.anidub.com/anime_tv/full/page/{number}/')
+    soup = BeautifulSoup(response.text, 'lxml')
+    anime_links = soup.select('div.lcol a[href*="/anime_tv/full/"]')
+    for link in anime_links:
+        title = link.text.strip()
+        if "Законченные сериалы" in title:
+            continue
+        url = link['href']
 
-for link in anime_links:
-    title = link.text.strip()
-    if "Законченные сериалы" in title:
-        continue
-    url = link['href']
+        anime_site = requests.get(f'{url}').content
+        soups = BeautifulSoup(anime_site, 'lxml')
+        file = soups.find('div', class_='torrent_h').find_all('a')
+        title_name = soups.find('span', id='news-title')
 
-    anime_site = requests.get(f'{url}').content
-    soups = BeautifulSoup(anime_site, 'lxml')
-    file = soups.find('div', class_='torrent_h').find_all('a')
-    title_name = soups.find('span', id='news-title')
+        def clean_filename(filename):
+            invalid_chars = r'[\\/*?:"<>|]'
+            cleaned = re.sub(invalid_chars, '!', filename)
+            cleaned = cleaned.replace(' ', '_')
+            return cleaned
 
-    def clean_filename(filename):
-        invalid_chars = r'[\\/*?:"<>|]'
-        cleaned = re.sub(invalid_chars, '!', filename)
-        cleaned = cleaned.replace(' ', '_')
-        return cleaned
-
-    for files in file:
-        href = files['href']
-        with open(f'{clean_filename(title_name.text)}.torrent', 'wb') as f:
-            f.write(requests.get(f"https://tr.anidub.com{href}").content)
-
+        for files in file:
+            href = files['href']
+            with open(f'{clean_filename(title_name.text)}.torrent', 'wb') as f:
+                f.write(requests.get(f"https://tr.anidub.com{href}").content)
+    number += 1
