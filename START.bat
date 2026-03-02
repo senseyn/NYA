46ed20d5cd496d99091828e9ee3baad99417a705
senseyn <novik_alex06@mail.ru>
2026-03-03 00:38:11 +0300

    NEW BAT

--- START.bat
+++ START.bat
@@ -2,10 +2,21 @@
 chcp 65001 > nul
 echo ЗАПУСК ЛОКАЛЬНОГО СЕРВЕРА
 
+echo --создание виртуального окружения--
+if not exist ".venv" (
+    echo [INFO] Создание виртуального окружения...
+    python -m venv .venv
+) else (
+    echo [INFO] Виртуальное окружение уже существует.
+)
+
 echo --активация venv--
 call .venv\Scripts\activate
 
-echo --запуск приложени--
+echo --установка библиотек--
+call pip install -r requirements.txt
+
+echo --запуск веб приложени--
 python app.py
 
 pause
